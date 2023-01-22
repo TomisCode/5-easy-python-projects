76c174b658236fbe6118801603347192814bc967
TomisCode <anddyb671ggmail.com>
2023-01-22 19:04:33 +0100

    The folder scan script has been modified

--- Folder_Scan.py
+++ Folder_Scan.py
@@ -12,7 +12,7 @@ results = ""
 os.chdir(scan_folder)
 
 #The file the program is going to find a match for
-image_file_to_match_hash = ""
+image_file_to_match_hash = " "
 
 #The for loop is going through all files in the folder with a .jpg extension
 for image_file_to_match in glob.glob("*.jpg"):
@@ -25,7 +25,7 @@ for image_file_to_match in glob.glob("*.jpg"):
             image_scan_hash = imagehash.average_hash(
                 (Image.open(image_scan)))
             
-            if image_file_to_match == image_scan_hash:
+            if image_file_to_match_hash == image_scan_hash:
                 results = results + \
                     "{0},{1},{2}\n".format(
                         image_scan, image_scan_hash, image_file_to_match)
